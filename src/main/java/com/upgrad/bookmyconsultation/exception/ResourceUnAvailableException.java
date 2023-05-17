8c62569a5ae135e0568f628d163a1f6ecb2163bc
Vishwa2702 <patelvishwa89@gmail.com>
2023-05-17 12:39:02 +0530

    Completed Capstone Project of bookmyconsultation backend

---
+++ src/main/java/com/upgrad/bookmyconsultation/exception/ResourceUnAvailableException.java
@@ -0,0 +1,8 @@
+package com.upgrad.bookmyconsultation.exception;
+
+public class ResourceUnAvailableException extends RuntimeException{
+
+    public ResourceUnAvailableException(String msg){
+        super(msg);
+    }
+}
